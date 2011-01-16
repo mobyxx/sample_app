baf71f012a9286c2d4f82fcd9ccbc97c4c6bc1d8
moby <rhabal@pomogo.com>
2011-01-16 11:35:28 -0800

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ SampleApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+ map.root controller:=>"pages"
 
   # See how all your routes lay out with "rake routes"
 
